aa6960819655e114f1046ffb516b7da7191f36b7
poulezh <ezh1986@inbox.ru>
2021-12-07 01:35:16 +0700

    part one

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div class="app">
+	<post-form
+		@create="createPost"
+	/>
+	<post-list 
+		:posts="posts"
+		
+	/>
+
+</div>
 </template>
+<script>
+import PostForm from '@/components/PostForm';
+import PostList from '@/components/PostList';
 
+
+
+export default {
+	components: {
+		PostForm, PostList,
+	},
+	data() {
+		return {
+			posts: [
+				{id: 1, title: 'js one', body: 'description 1'},
+				{id: 2, title: 'js two', body: 'description 2'},
+				{id: 3, title: 'js three', body: 'description 2'},
+				{id: 4, title: 'js four', body: 'description 3'},
+			],
+		}
+	},
+	methods: {
+		createPost(post) {
+			this.posts.push(post);
+		}
+
+	}
+}
+
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+ *{
+	 margin: 0;
+	 padding: 0;
+	 box-sizing: border-box;
+ }
+ .app{
+	 padding:20px;
+ }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,72 @@
+<template>
+	<form @submit.prevent>
+		<h4>Creating Post</h4>
+		<input 
+			v-model="post.title"
+			class="input" 
+			type="text" 
+			placeholder="name"
+		>
+		<input 
+			v-model="post.body"
+			class="input" 
+			type="text" 
+			placeholder="desc"
+		>
+		<button 
+			class="btn" 
+			@click="createPost"
+			>
+			Create post
+		</button>
+	</form>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				post: {
+					title: '',
+					body: '',
+
+				}
+			}
+		},
+		methods: {
+			createPost () {
+				this.post.id = Date.now();
+				this.$emit('create', this.post)
+				this.post = {
+					title : '',
+					body : '',
+				}
+			}
+		}
+		
+	}
+</script>
+
+<style lang="css" scoped>
+form{
+	display: inline-flex;
+	flex-direction: column;
+	border: 2px solid teal;
+	padding: 15px;
+	
+}
+.input {
+	width: 350px;
+	border: 2px solid teal;
+	padding:  10px 15px;
+	margin-top: 15px;
+}
+.btn {
+	width: 220px;
+	padding: 10px;
+	margin-top: 25px;
+	text-transform: uppercase;
+	align-self: center;
+	cursor: pointer;
+}
+</style>
