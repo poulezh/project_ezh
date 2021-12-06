25eab5d801e2324b191f563895944535bdaf0c69
poulezh <ezh1986@inbox.ru>
2021-12-07 01:37:04 +0700

    part two

---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="post" v-for="post in posts">
+		<div> name post: <strong>{{ post.title  }}</strong> </div>
+		<div> desc post: <strong>{{ post.body  }}</strong> </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props:{
+			posts:{
+				type: Array,
+				required: true,
+			}}
+		
+	}
+</script>
+
+<style scoped>
+.post {
+	padding: 15px;
+	border: 2px solid teal;
+	margin-top: 15px;
+	}
+
+</style>
